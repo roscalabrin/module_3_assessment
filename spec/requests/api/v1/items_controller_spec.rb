51ce9edcfbb9bbfedf8d1b2ac8eab59741cb56a9
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-20 11:03:21 -0600

    request tests

--- spec/requests/api/v1/items_controller_spec.rb
+++ spec/requests/api/v1/items_controller_spec.rb
@@ -1,10 +1,33 @@
 require 'rails_helper'
 
-Rspec.describe Api::V1::ItemsController, :type => :request do
+RSpec.describe Api::V1::ItemsController, :type => :request do
+  
   describe "GET #index" do
-    
-    get 'api/vi/items'
-    expect(response).to be_sucess
-    
+    it "returns all items" do
+      item1 = Item.create(name: "Incredible Iron Chair")
+      item2 = Item.create(name: "Rustic Marble Hat")
+      
+      get 'api/v1/items'
+      
+      expect(response).to be_sucess
+      
+      #  I receive a 200 JSON response containing all items 
+      #  And each item has an id, name, description, and image_url but not the created_at or updated_at
+    end
   end
+  
+  describe "GET #show" do
+    it "returns all items" do
+      item = Item.create(name: "Incredible Iron Chair")
+      item_id = item.id
+      
+      get "api/v1/items/#{item_id}"
+      
+      expect(response).to be_sucess
+      
+      #  I receive a 200 JSON response containing all items 
+      #  And each item has an id, name, description, and image_url but not the created_at or updated_at
+    end
+  end
+
 end
