63d336597c2a0a43cc0ae7dd13f8fee96be3c85b
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-20 10:32:10 -0600

    edits to search view

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,11 +1,11 @@
 class Store
-    def initialize(raw_data)
-      require "pry"
-      binding.pry
-      # @long_name = 
-      # @city = 
-      # @distance = 
-      # @phone = 
-      # @type = 
-    end
+  attr_reader :name, :city, :distance, :phone, :type
+  
+  def initialize(raw_data)
+    @name = raw_data[:longName]
+    @city      = raw_data[:city]
+    @distance  = raw_data[:distance]
+    @phone     = raw_data[:phone]
+    @type      = raw_data[:storeType]
+  end
 end
--- spec/features/user_can_search_store_by_zipcode_spec.rb
+++ spec/features/user_can_search_store_by_zipcode_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature "User can search by zipcode" do
   scenario "User sees a list of stores within 25 miles from a certain zipcode" do
-    # VCR.use_cassette("stores") do
+    VCR.use_cassette("search_by_zip_code") do
       visit '/'
       
       fill_in "zip_code", with: "80202"
@@ -11,13 +11,13 @@ feature "User can search by zipcode" do
       expect(current_path).to eq('/search')
       expect(page).to have_content "17 Total Stores"
       expect(page).to have_selector(".search-results", count: 15)
-      within(".search-results:nth-child(1)") do
+      within(".search-results:first") do
         expect(page).to have_content "Best Buy Mobile - Cherry Creek Shopping Center"
         expect(page).to have_content "Denver"
         expect(page).to have_content 3.25
         expect(page).to have_content "303-270-9189"
         expect(page).to have_content store type "Mobile"
       end
-    # end
+    end
   end
 end
